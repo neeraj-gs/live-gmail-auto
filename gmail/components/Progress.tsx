ed9e99b84126fa844a96f285dd1c0a02e50033ff
neeraj-gs <gsneeraj05@gmail.com>
2024-01-09 12:04:36 +0530

    Added Video also for the Progress component and completed the implementation part of it

--- gmail/components/Progress.tsx
+++ gmail/components/Progress.tsx
@@ -38,7 +38,7 @@ const Progress = () => {
         height={340} // Set height as per your requirement
         className="mt-4 shadow-lg"
       >
-        <source src="/" type="video/mp4" />
+        <source src="/Gmail.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 

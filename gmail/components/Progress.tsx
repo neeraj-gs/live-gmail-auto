849a6fc63c280f75e93ba15ab441024eb3789bb3
neeraj-gs <gsneeraj05@gmail.com>
2024-01-09 10:03:05 +0530

    Added a Basic UI need to add the video to it

--- gmail/components/Progress.tsx
+++ gmail/components/Progress.tsx
@@ -22,9 +22,9 @@ const Progress = () => {
   return (
     <>
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-      <Image className="mx-auto" src="/construction.gif" alt="Work in progress image" height={50} width={50} />
+      {/* <Image className="mx-auto" src="/construction.gif" alt="Work in progress image" height={50} width={50} /> */}
       <h1 className='font-semibold text-lg text-center mt-2'>
-        A Node.js Backend Project<br /> <span className="font-bold text-green-600"> Automatically Replies</span> to Mails When User is Unable to send mails &nbsp; <br />
+        A Node.js Backend Project that<br /> <span className="font-bold text-green-600"> Automatically Replies</span> to Mails When User is Unable to send mails &nbsp; <br />
         <h2 className="mt-12 mb-12 font-bold text-blue-600">
             <TypeWriterTitle />
         </h2>
@@ -46,7 +46,7 @@ const Progress = () => {
       
 
       <footer className="mt-20 ml-16 font-bold">
-        <a className="underline" href="https://github.com/neeraj-gs/Gmail-Auto-Reply" target="_blank">Download Source Code</a>
+        <a className="underline text-blue-500" href="https://github.com/neeraj-gs/Gmail-Auto-Reply" target="_blank">Download Source Code</a>
         &nbsp;&nbsp;&nbsp;| &nbsp; Created With 💚 By <a href="https://github.com/neeraj-gs" target="_blank" className="underline">Neeraj G S</a> 
       </footer>
       

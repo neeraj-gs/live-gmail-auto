7dcc5272a166c09bd18d9c11ad769f6ab06f157e
neeraj-gs <gsneeraj05@gmail.com>
2024-01-09 09:56:13 +0530

    Added a Componet and also added a favicon

--- gmail/app/page.tsx
+++ gmail/app/page.tsx
@@ -1,6 +1,15 @@
+import Progress from "@/components/Progress";
+import Image from "next/image";
+import logo from '../public/favicon.png'
 
 export default function Home() {
   return (
-    <div>Hii</div>
+    <div className="bg-gradient-to-r min-h-screen grainy from-rose-50 to-slate-100">
+      <div className='p-10'>
+      <Image  src={logo} alt='Logo' width={70} height={70} />
+      </div>
+      
+      <Progress />
+    </div>
   )
 }
---
+++ gmail/components/Progress.tsx
@@ -0,0 +1,10 @@
+
+type Props = {}
+
+const Progress = (props: Props) => {
+  return (
+    <div>Progress</div>
+  )
+}
+
+export default Progress
